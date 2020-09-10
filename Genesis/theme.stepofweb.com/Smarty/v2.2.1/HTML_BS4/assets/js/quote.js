7ca1c5a4daf8003f361f275b2880d9011f988d96
Fabien H. Dimitrov <dimitrov.fabien@gmail.com>
2020-09-09 23:48:22 -0400

    Finish quote form & validation through jQuery

--- Genesis/theme.stepofweb.com/Smarty/v2.2.1/HTML_BS4/assets/js/quote.js
+++ Genesis/theme.stepofweb.com/Smarty/v2.2.1/HTML_BS4/assets/js/quote.js
@@ -9,20 +9,27 @@
 $(document).ready(function(){
     
 
-    var totalElevators = 0;   // Estimated number of elevator shafts
-    var buildingType = ""; 
-
+    var buildingType = "";      // Building type
+      
+    var totalElevators = 0;     // Estimated number of elevator shafts
+    var elevatorsCost = 0;      // Elevators cost, based on product line (Stnd, Premium, Excelium) without install fee
+    var installationCost = 0;   // Installation cost, based on product line
+    var totalCost = 0;          // Total estimate cost
 
     /* Do this after client has clicked on the Confirm button ***/
     var confirmBtn = $("#building-confirm-btn");
     confirmBtn.click(function(){
 
+        // Show results section
+        var resultsDiv = $("#results");
+        resultsDiv.show();
+
         // Get building type
         var building = $("#building-type").val();
         buildingType = building;
 
-        // Declare empty array: stores the input divs 
-        var inputDivs = [];
+        var inputDivs = []; // Stores the input divs 
+        var inputs = []; // Stores the inputs
         
         // Get six input divs and put them in the inputDivs array
         var q1 = $("#q1");
@@ -34,14 +41,21 @@ $(document).ready(function(){
 
         inputDivs.push(q1, q2, q3, q4, q5, q6);
 
+        // Get six inputs from the 6 input fields and put them in the inputs array
+        var input1 = $("#question-1");
+        var input2 = $("#question-2");
+        var input3 = $("#question-3");
+        var input4 = $("#question-4");
+        var input5 = $("#question-5");
+        var input6 = $("#question-6");
+
+        inputs.push(input1, input2, input3, input4, input5, input6);
+
         // Reset all input fields when button is pressed, just in case it is pressed more than once
-        $("#question-1").val(0);
-        $("#question-2").val(0);
-        $("#question-3").val(0);
-        $("#question-4").val(0);
-        $("#question-5").val(0);
-        $("#question-6").val(0);
-        $("#num-shafts").val(0);
+        for (var i = 0; i < inputs.length; i++)
+        {
+            inputs[i].val(NaN);
+        }
 
         // Get labels for the six input divs and put them in the divLabels array
         var q1Label = $("#q1-label");
@@ -103,8 +117,13 @@ $(document).ready(function(){
             inputDivs[i].hide();
         }
 
+        // Resets results if 'Confirm' button is pressed again
+        $("#num-shafts").val(0);
+        $("#total-estimate").val(0);
+
     })
 
+    // Runs every 0.5 seconds
     window.setInterval (function () {
         if (buildingType === "Residential")
         {
@@ -117,7 +136,6 @@ $(document).ready(function(){
             A new column is therefore added to each new group of 20 stories.
             */
 
-            console.log("residential is here")
             var numApartments = $("#question-1").val();
             var numFloors = $("#question-2").val() - $("#question-3").val();
             var avgApartmentsPerFloor = Math.ceil(numApartments / numFloors);
@@ -128,7 +146,6 @@ $(document).ready(function(){
         }
         else if (buildingType === "Commercial")
         {
-            console.log("commercial is here")
             /*
             If the type of building is Commercial, the number of elevator shafts 
             to be deployed is specified and the estimated number of cages is equal to 
@@ -138,7 +155,6 @@ $(document).ready(function(){
         }
         else 
         {
-            console.log("corporate/hybrid is here");
             /* 
             If the type of building is Corporate or Hybrid, multiply the number of occupants per floor 
             by the number of floors (including the number of basements) to obtain the total number 
@@ -149,65 +165,45 @@ $(document).ready(function(){
             number of elevators per column. The total number of elevators is determined by 
             the number of elevators per column multiplied by the number of columns.
             */
-            var numOccupantsPerFloor = $("#question-5").val(); // 240
-            console.log("Num occupants per floor: " + numOccupantsPerFloor);
-            var numFloors = $("#question-2").val(); // 95
-            console.log("Num floors: " + numFloors);
-            var totalOccupants = numOccupantsPerFloor * numFloors; // 240 x 95 = 22800
-            console.log("Total occupants: " + totalOccupants);
-            var numElevators = Math.ceil(totalOccupants / 1000); // 22800 / 1000 = 22.8 = 23
-            console.log("Num elevators: " + numElevators);
-            var numColumns = Math.ceil(numFloors / 20); // 95/20 = 4.75 = 5
-            console.log("Num columns:" + numColumns);
+            var numOccupantsPerFloor = $("#question-5").val(); 
+            var numFloors = $("#question-2").val(); 
+            var totalOccupants = numOccupantsPerFloor * numFloors;
+            var numElevators = Math.ceil(totalOccupants / 1000);
+            var numColumns = Math.ceil(numFloors / 20); 
             
-            totalElevators = Math.ceil(numElevators / numColumns) * numColumns; // 23/5 * 5 = 5 * 5 = 25 
-            console.log("Total elevators" + totalElevators);
+            totalElevators = Math.ceil(numElevators / numColumns) * numColumns;
         }
-        console.log(totalElevators);
         
-        
+        // Prevents NaN errors from displaying on the console when the estimate hasnt been calculated yet
         if (!isNaN(totalElevators))
             $("#num-shafts").val(totalElevators);
         
-        
-        $("input").keyup(function() {
-            if ($("input").val() < 0)
-            {
-                $("input").val(0);
-            }
-        })
-    }, 1000);
-    
-
-    
-
-    /*
-    $('form[id="quote-form"]').validate({
-        rules: {
-          fname: 'required',
-          lname: 'required',
-          user_email: {
-            required: true,
-            email: true,
-          },
-          psword: {
-            required: true,
-            minlength: 8,
-          }
-        },
-        messages: {
-          fname: 'This field is required',
-          lname: 'This field is required',
-          user_email: 'Enter a valid email',
-          psword: {
-            minlength: 'Password must be at least 8 characters long'
-          }
-        },
-        submitHandler: function(form) {
-          form.submit();
+        // Get product line choice and calculate cost
+        var productLine = $("input[name='product-line']:checked").val();
+        if (productLine === "standard")
+        {
+            elevatorsCost = totalElevators * 7565;
+            installationCost = elevatorsCost * 0.1;
         }
-      });
-    */
+        else if (productLine === "premium") 
+        {
+            elevatorsCost = totalElevators * 12345;
+            installationCost = elevatorsCost * 0.13;
+        }
+        else if (productLine === "excelium")
+        {
+            elevatorsCost = totalElevators * 15400;
+            installationCost = elevatorsCost * 0.16;
+        }
 
-
+        // Display cost
+        $("#unit-price").val(elevatorsCost.toFixed(2));
+        $("#installation-price").val(installationCost.toFixed(2));
+        if (!isNaN(elevatorsCost) && !isNaN(installationCost))
+        {
+            totalCost = elevatorsCost + installationCost;
+            $("#total-estimate").val(totalCost.toFixed(2));
+        }
+        
+    }, 500);
   });
